509b7b03a12d9bec6be1a43cee83ceea13426ced
kb19180 <160750238+kb19180@users.noreply.github.com>
2024-04-29 15:08:38 +0100

    Update check-db.php

--- SoftEng_Submission/check-db.php
+++ SoftEng_Submission/check-db.php
@@ -1,36 +1,31 @@
 <?php
-// Database connection parameters
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "kiosk";
 
-// Establishing a connection to the database
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check the connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Assuming form data is submitted via POST method
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get email and password from the login form
     $email = $_POST['uname'];
     $password = $_POST['psw'];
 
-    // Prepare SQL statement to retrieve user data
+    
     $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // User found, fetch user data
         $user = $result->fetch_assoc();
-        
-        // Retrieve the user's usertype
         $usertype = $user['userType'];
-
-        // Redirect based on usertype
+        
         switch ($usertype) {
             case 'Manager':
                 header("Location: manager.php");
@@ -46,6 +41,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Close the database connection
 $conn->close();
 ?>
